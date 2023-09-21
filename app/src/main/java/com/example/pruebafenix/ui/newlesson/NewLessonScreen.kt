daadc89dac345fa435d5e7902a78751bafd46367
Cristipi <cristipigm@gmail.com>
2023-09-21 19:40:54 +0200

    nueva pantalla de newlesson.

--- app/src/main/java/com/example/pruebafenix/ui/newlesson/NewLessonScreen.kt
+++ app/src/main/java/com/example/pruebafenix/ui/newlesson/NewLessonScreen.kt
@@ -1,144 +1,91 @@
 package com.example.pruebafenix.ui.newlesson
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.pruebafenix.R
 
 @Composable
 fun NewLessonScreen(
     navController: NavController
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column {
         Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.End) {
+            horizontalArrangement = Arrangement.End)
+        {
             IconButton(onClick = { navController.popBackStack() }) {
-                Icon(Icons.Filled.Close,
-                    "Close")
+                Icon(
+                    Icons.Filled.Close,
+                    "Close",
+                    tint = MaterialTheme.colorScheme.primary)
             }
         }
-        Spacer(modifier = Modifier
-            .fillMaxWidth()
-            .height(10.dp))
-        ItemsLesson("Nombre de la clase:")
-        ItemsLesson("Hora inicio:")
-        ItemsLesson("Hora fin:")
-        ItemsLesson("Numero de plazas:")
-
-        CustomTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = Color(0xffb9b9b9),
-                    shape = RoundedCornerShape(percent = 10)
-                )
-                .padding(horizontal = 12.dp, vertical = 10.dp),
-            leadingIcon = { Icon(Icons.Filled.Email, "Email") },
-            trailingIcon = { Icon(Icons.Filled.Email, "Email") },
-            paddingLeadingIconEnd = 10.dp,
-            paddingTrailingIconStart = 10.dp
-        )
-
-        Row(
-            Modifier
-                .background(
-                    colorResource(id = R.color.grey)
-                )
-                .padding(10.dp)
-        ) {
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 20.dp, end = 20.dp).background(Color.White, RoundedCornerShape(22.dp)),
-                shape = RoundedCornerShape(22.dp),
-                value = "",
-                onValueChange = {},
-                textStyle = MaterialTheme.typography.bodyLarge
-            )
-        }
+        ItemLesson("Nombre clase:")
+        ItemLesson("Hora de inicio:")
+        ItemLesson("Hora fin:")
     }
 }
 
 @Composable
-fun ItemsLesson(
-    titleName: String
+private fun ItemLesson(
+    text: String
 ) {
-    Row(modifier = Modifier
+    Column (modifier = Modifier
         .fillMaxWidth()
-        .padding(10.dp)) {
-        Text(text = titleName,
-            modifier = Modifier.weight(1f))
-        TextField(value = "", onValueChange = {},
-            modifier = Modifier
-                .weight(1f)
-                .background(
-                color = Color(0xffb9b9b9),
-                shape = RoundedCornerShape(percent = 10)
-            ))
+        .padding(bottom = 10.dp))
+    {
+        Text(text = text,
+            fontSize = 20.sp,
+            color = MaterialTheme.colorScheme.primary)
+        BasicTextField(
+            value = "value",
+            onValueChange = { },
+            textStyle = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colorScheme.primary
+            ),
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                            width = 2.dp,
+                            color = MaterialTheme.colorScheme.primary,
+                            shape = RoundedCornerShape(size = 16.dp)
+                        )
+                        .padding(horizontal = 16.dp, vertical = 12.dp), // inner padding
+                ) {
+                    innerTextField()
+                }
+            }
+        )
     }
 }
 
-@Composable
-private fun CustomTextField(
-    modifier: Modifier = Modifier,
-    paddingLeadingIconEnd: Dp = 0.dp,
-    paddingTrailingIconStart: Dp = 0.dp,
-    leadingIcon: (@Composable() () -> Unit)? = null,
-    trailingIcon: (@Composable() () -> Unit)? = null
-) {
-    //val state = savedInstanceState(saver = TextFieldValue.Saver) { TextFieldValue() }
-
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        if (leadingIcon != null) {
-            leadingIcon()
-        }
-        Box(
-            modifier = Modifier.weight(1f)
-                .padding(start = paddingLeadingIconEnd, end = paddingTrailingIconStart)
-        ) {
-            TextField(
-                modifier = Modifier.background(
-                    color = Color(0xffb9b9b9),
-                    shape = RoundedCornerShape(percent = 10)
-                ),
-                value = "HOLA",
-                onValueChange = { //state.value = it
-             }
-            )
-        }
-        if (trailingIcon != null) {
-            trailingIcon()
-        }
-    }
-}
 @Preview(showBackground = true)
 @Composable
-fun PreviewNewLesson() {
-    //NewLessonScreen()
+fun Preview() {
+    val nav = NavController(LocalContext.current)
+    NewLessonScreen(nav)
 }
